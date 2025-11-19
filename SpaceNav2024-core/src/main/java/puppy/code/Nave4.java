fb14ed5a63e320defbaaa4fb2e9817e398aee628
pipebc <felipebarrazacarvajal06@gmail.com>
2025-11-19 20:13:25 -0300

    strategy en nave4

--- SpaceNav2024-core/src/main/java/puppy/code/Nave4.java
+++ SpaceNav2024-core/src/main/java/puppy/code/Nave4.java
@@ -12,6 +12,16 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Nave4 extends EntidadJuego implements Destructible {
 	
+	public static final int ModoDisparoNormal = 0;
+    public static final int ModoDisparoRapido = 1;
+    public static final int ModoDisparoRafaga = 2;
+    private int modoDisparo = ModoDisparoNormal;
+    
+    private float cooldownNormal = 0.5f;
+    private float cooldownRafaga = 0.15f;
+    private float tiempoDesdeUltimoDisparo = 0f;
+    private int rafagaRestante = 0;
+	
 	private int vidas;
     private boolean herido;
     private float tiempoHerido;
@@ -37,7 +47,6 @@ public class Nave4 extends EntidadJuego implements Destructible {
 
     }
     
-    
     @Override
     public void update(float delta) {
         if (destroyed) return;
@@ -59,11 +68,35 @@ public class Nave4 extends EntidadJuego implements Destructible {
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             xSpeed = velocidad;
         }
-
         
-        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            disparar();
+        if (Gdx.input.isKeyJustPressed(Input.Keys.Q)) {
+            modoDisparo = (modoDisparo + 1) % 3;
         }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)) {
+            modoDisparo = ModoDisparoNormal;
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)) {
+            modoDisparo = ModoDisparoRapido;
+        }
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_3)) {
+            modoDisparo = ModoDisparoRafaga;
+        }
+
+        tiempoDesdeUltimoDisparo -= delta;
+        switch (modoDisparo) {
+	        case ModoDisparoNormal:
+	            manejarDisparoNormal(delta);
+	            break;
+	        case ModoDisparoRapido:
+	            manejarDisparoRapido(delta);
+	            break;
+	        case ModoDisparoRafaga:
+	            manejarDisparoRafaga(delta);
+	            break;
+	        default:
+	            manejarDisparoNormal(delta);
+	            break;
+	    }
 
         
         float newX = getX() + (xSpeed * delta);
@@ -93,7 +126,7 @@ public class Nave4 extends EntidadJuego implements Destructible {
             }
         }
     }
-      
+    
     private void disparar() {
         
         float balaX = getX() + getWidth() / 2 - txBala.getWidth() / 2;
@@ -156,4 +189,43 @@ public class Nave4 extends EntidadJuego implements Destructible {
     public boolean estaHerido() {
         return herido;
     }
+    
+    public void setModoDisparo(int modo) {
+        if (modo >= ModoDisparoNormal && modo <= ModoDisparoRafaga) {
+            this.modoDisparo = modo;
+        }
+    }
+
+    public int getModoDisparo() {
+        return this.modoDisparo;
+    }
+    
+    private void manejarDisparoNormal(float delta) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && tiempoDesdeUltimoDisparo <= 0f) {
+            disparar();
+            tiempoDesdeUltimoDisparo = cooldownNormal;
+        }
+    }
+
+    private void manejarDisparoRapido(float delta) {
+        float cooldown = cooldownNormal * 0.4f;
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && tiempoDesdeUltimoDisparo <= 0f) {
+            disparar();
+            tiempoDesdeUltimoDisparo = cooldown;
+        }
+    }
+
+    private void manejarDisparoRafaga(float delta) {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE) && rafagaRestante == 0) {
+            rafagaRestante = 3;
+            tiempoDesdeUltimoDisparo = 0f;
+        }
+        if (rafagaRestante > 0 && tiempoDesdeUltimoDisparo <= 0f) {
+            disparar();
+            rafagaRestante--;
+            tiempoDesdeUltimoDisparo = cooldownRafaga;
+        }
+    }
 }
+
+
